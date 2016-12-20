ca823d512a79abb414fc1d88fefe56cb5b7bdaa8
Bill Dall <bdall@teradici.com>
2016-12-20 13:14:28 -0800

    Updating agent install script to pull from correct location and not install if there's already and agent installed.

--- dev/domain-controller/new-vm-join-domain/Install-PCoIPAgent.ps1
+++ dev/domain-controller/new-vm-join-domain/Install-PCoIPAgent.ps1
@@ -12,9 +12,15 @@ param (
 #Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
 
 
+if ( Get-Item -path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\PCoIP Standard Agent" -ErrorAction SilentlyContinue )
+{
+# an agent is already installed. Assume it's the right one and exit cleanly. TODO: also test for Graphics and Multi-Session
+    Exit
+}
 
-$source = "https://teradeploy.blob.core.windows.net/pcoipagent/PCoIP_agent_release_installer_2.6.2.3771_standard.exe"
-$dest = "C:\PCoIPAgentInstaller"
+
+$source = "https://teradeploy.blob.core.windows.net/binaries/PCoIP_agent_release_installer_2.6.2.3771_standard.exe"
+$dest = ".\PCoIPAgentInstaller"
 $installerFileName = "PCoIP_agent_release_installer_2.6.2.3771_standard.exe"
 New-Item -Path $dest -ItemType directory
 Invoke-WebRequest $source -OutFile "$dest\$installerFileName"
@@ -27,27 +33,26 @@ Stop-Service PCoIPAgent
 #install the agent
 & ".\$installerFileName" /S
 
+# standard agent will request restart, so don't wait (and if we make this a DSC then that will be automagic...)
+
+#old:
 #and wait until the service has started
 
-$running = (Get-Service PCoIPAgent | Select -ExpandProperty Status -first 1) -eq "Running";
-$loopCountRemaining = 1800; #seconds
+#$running = (Get-Service PCoIPAgent | Select -ExpandProperty Status -first 1) -eq "Running";
+#$loopCountRemaining = 1800; #seconds
 #loop until it's installed and running
-while($running -eq $false)
-{
-    if($loopCountRemaining % 10 -eq 0)
-    {
-        Write-Host "Waiting for Agent Install. Remaining: $loopCountRemaining"
-    }
+#while($running -eq $false)
+#{
+#    if($loopCountRemaining % 10 -eq 0)
+#    {
+#        Write-Host "Waiting for Agent Install. Remaining: $loopCountRemaining"
+#    }
 
-    Start-Sleep -Seconds 1;
-    $running = (Get-Service PCoIPAgent -ErrorAction SilentlyContinue | Select -ExpandProperty Status -first 1) -eq "Running";
-    $loopCountRemaining = $loopCountRemaining - 1
-    if ($loopCountRemaining -eq 0)
-    {
-        throw "Install failed or took really long!"
-    }
-}
-
-
-
-
+#    Start-Sleep -Seconds 1;
+#    $running = (Get-Service PCoIPAgent -ErrorAction SilentlyContinue | Select -ExpandProperty Status -first 1) -eq "Running";
+#    $loopCountRemaining = $loopCountRemaining - 1
+#    if ($loopCountRemaining -eq 0)
+#    {
+#        throw "Install failed or took really long!"
+#    }
+#}
