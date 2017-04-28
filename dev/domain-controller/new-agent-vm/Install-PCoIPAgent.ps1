cb28ebeaf2a49d3189a5b3236314f7e15fb01fb5
Hanif Ladha <hladha@teradici.com>
2017-04-28 11:51:22 -0700

    Added TestScript block to sumo install

--- dev/domain-controller/new-agent-vm/Install-PCoIPAgent.ps1
+++ dev/domain-controller/new-agent-vm/Install-PCoIPAgent.ps1
@@ -3,6 +3,40 @@ Configuration InstallPCoIPAgent
 {
     Node "localhost"
     {
+        File Sumo_Directory 
+        {
+            Ensure          = "Present"
+            Type            = "Directory"
+            DestinationPath = "C:\sumo"
+        }
+        # Aim to install the collector first and start the log collection before any 
+        # other applications are installed.
+        Script Install_SumoCollector
+        {
+            DependsOn  = "[File]Sumo_Directory"
+            GetScript  = { @{ Result = "Install_SumoCollector" } }
+
+            TestScript = { return $false }
+
+            SetScript  = {
+                Write-Verbose "Install_SumoCollector"
+
+                $sumo_package = "https://teradeploy.blob.core.windows.net/binaries/SumoCollector-windows-x64_19_182-25.exe"
+                $sumo_config = "/new-admin-vm/sumo.conf"
+                $sumo_collector_json = "/new-admin-vm/sumo-admin-vm.json"
+                $dest = "C:\sumo"
+                Invoke-WebRequest $sumo_config -OutFile "$dest\sumo.conf"
+                Invoke-WebRequest $sumo_collecor_json -OutFile "$dest\sumo-admin-vm.json"
+	        	# Insert unique ID
+        		(Get-Content "$dest\sumo.conf").Replace("collectorID", $using:sumoCollectorID) | Set-Content "$dest\sumo.conf"
+                
+                $installerFileName = "SumoCollector-windows-x64_19_182-25.exe"
+		        Invoke-WebRequest $sumo_package -OutFile "$dest\$installerFileName"
+                #install the collector
+                & "$dest\$installerFileName" /S
+            }
+        }
+
         LocalConfigurationManager
         {
             RebootNodeIfNeeded = $true
@@ -33,38 +67,6 @@ Configuration InstallPCoIPAgent
                 Invoke-WebRequest $source -OutFile "$dest\$installerFileName"
 
                 #install the agent
-                & "$dest\$installerFileName" /S
-            }
-        }
-
-        File Sumo_Directory 
-        {
-            Ensure          = "Present"
-            Type            = "Directory"
-            DestinationPath = "C:\sumo"
-        }
-        # Aim to install the collector first and start the log collection before any 
-        # other applications are installed.
-        Script Install_SumoCollector
-        {
-            DependsOn  = "[File]Sumo_Directory"
-            GetScript  = { @{ Result = "Install_SumoCollector" } }
-
-            SetScript  = {
-                Write-Verbose "Install_SumoCollector"
-
-                $sumo_package = "https://teradeploy.blob.core.windows.net/binaries/SumoCollector-windows-x64_19_182-25.exe"
-                $sumo_config = "/new-admin-vm/sumo.conf"
-                $sumo_collector_json = "/new-admin-vm/sumo-admin-vm.json"
-                $dest = "C:\sumo"
-                Invoke-WebRequest $sumo_config -OutFile "$dest\sumo.conf"
-                Invoke-WebRequest $sumo_collecor_json -OutFile "$dest\sumo-admin-vm.json"
-		# Insert unique ID
-		#(Get-Content "$dest\sumo.conf").Replace("collectorID", $using:sumoCollectorID) | Set-Content "$dest\sumo.conf"
-                
-                $installerFileName = "SumoCollector-windows-x64_19_182-25.exe"
-		Invoke-WebRequest $sumo_package -OutFile "$dest\$installerFileName"
-                #install the collector
                 & "$dest\$installerFileName" /S
             }
         }
