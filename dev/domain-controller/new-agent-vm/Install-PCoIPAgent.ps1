892d63a8b9e28ed6c5fbe6379e01091d2b6a695f
Pravjot Rai <prai@teradici.com>
2017-05-01 14:45:27 -0700

    Testing collector deployment

--- dev/domain-controller/new-agent-vm/Install-PCoIPAgent.ps1
+++ dev/domain-controller/new-agent-vm/Install-PCoIPAgent.ps1
@@ -22,10 +22,8 @@ Configuration InstallPCoIPAgent
                 Write-Verbose "Install_SumoCollector"
 
                 $sumo_package = "https://teradeploy.blob.core.windows.net/binaries/SumoCollector_windows-x64_19_182-25.exe"
-		$sumo_config = "https://raw.githubusercontent.com/teradici/deploy/sumo/dev/domain-controller/new-agent-vm/sumo.conf"
-		$sumo_collector_json = "https://raw.githubusercontent.com/teradici/deploy/sumo/dev/domain-controller/new-agent-vm/sumo-agent-vm.conf"
-		#$sumo_config = "$using:gitLocation/sumo.conf"	
-		#$sumo_collector_json= "$using:gitLocation/sumo-agent-vm.conf"	
+		$sumo_config = "$using:gitLocation/sumo.conf"	
+		$sumo_collector_json= "$using:gitLocation/sumo-agent-vm.conf"	
 		#$sumo_config = "/new-admin-vm/sumo.conf"
                 #$sumo_collector_json = "/new-admin-vm/sumo-admin-vm.json"
                 $dest = "C:\sumo"
