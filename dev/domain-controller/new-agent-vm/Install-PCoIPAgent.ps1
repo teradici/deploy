695911c2519b3e85305cf7222aac159c3668fb74
Pravjot Rai <prai@teradici.com>
2017-05-02 15:11:36 -0700

    Added direct url to agent install step for debugging purposes

--- dev/domain-controller/new-agent-vm/Install-PCoIPAgent.ps1
+++ dev/domain-controller/new-agent-vm/Install-PCoIPAgent.ps1
@@ -22,11 +22,13 @@ Configuration InstallPCoIPAgent
                 Write-Verbose "Install_SumoCollector"
 
                 $sumo_package = "https://teradeploy.blob.core.windows.net/binaries/SumoCollector_windows-x64_19_182-25.exe"
-		$sumo_config = "$gitLocation/sumo.conf"	
-		$sumo_collector_json= "$gitLocation/sumo-agent-vm.conf"	
+#	$sumo_config = "$gitLocation/sumo.conf"	
+#		$sumo_collector_json= "$gitLocation/sumo-agent-vm.conf"	
 		#$sumo_config = "/new-agent-vm/sumo.conf"
 		#$sumo_collector_json = "/new-agent-vm/sumo-agent-vm.json"
-                $dest = "C:\sumo"
+                $sumo_config = "https://raw.githubusercontent.com/teradici/deploy/sumo/dev/domain-controller/new-agent-vm/sumo.conf"
+                $sumo_collector_json = "https://raw.githubusercontent.com/teradici/deploy/sumo/dev/domain-controller/new-agent-vm/sumo-agent-vm.json"
+		$dest = "C:\sumo"
 		Invoke-WebRequest -Uri $sumo_config -PassThru -OutFile "$dest\sumo.conf"
 		Invoke-WebRequest -Uri $sumo_collector_json -PassThru -OutFile "$dest\sumo-agent-vm.json"
 	        #Insert unique ID
