0ca6d9fa9490fea8bab51dd6e064de2c477c790d
Frank Wang <fwang@teradici.com>
2017-05-14 10:42:29 -0700

    TSW-62495 regenerate certificate

--- dev/domain-controller/FinalizeDeployment.ps1
+++ dev/domain-controller/FinalizeDeployment.ps1
@@ -43,8 +43,6 @@ $certPath = $certLoc + '\' + $cert.Thumbprint
 $certPfx = 'C:\WindowsAzure\mySelfSignedCert.pfx'
 $randomPswd = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 16 | % {[char]$_})
 
-$randomPswd | out-file -FilePath 'c:\WindowsAzure\passwd.txt'
-
 $certPwd = ConvertTo-SecureString -String $randomPswd -AsPlainText -Force
 Export-PfxCertificate -Cert $certPath -FilePath $certPfx -Password $certPwd
 
@@ -68,3 +66,33 @@ $parameters.Add(“certData”, "$fileContentEncoded")
 $parameters.Add(“certPassword”, "$randomPswd")
 
 New-AzureRmResourceGroupDeployment -Mode Incremental -Name "DeployAppGateway" -ResourceGroupName $rgName -TemplateUri $templateUri -TemplateParameterObject $parameters
+
+
+#regenerate cert with the fqdn
+$fqdn = (Get-AzureRmPublicIpAddress -ResourceGroupName $rgName -Name publicip1).DnsSettings.Fqdn
+
+$certLoc = 'cert:Localmachine\My'
+$startDate = [DateTime]::Now.AddDays(-1)
+
+$subject = $fqdn + ",O=Teradici Corporation,OU=SoftPCoIP,L=Burnaby,ST=BC,C=CA"
+
+$cert = New-SelfSignedCertificate -certstorelocation $certLoc -Subject $subject -KeyLength 3072 -FriendlyName "PCoIP Application Gateway" -NotBefore $startDate -TextExtension @("2.5.29.19={critical}{text}ca=1") -HashAlgorithm SHA384 -KeyUsage DigitalSignature, CertSign,  CRLSign, KeyEncipherment
+
+$certPath = $certLoc + '\' + $cert.Thumbprint
+$certPfx = 'C:\temp\localcert\mySelfSignedCert.pfx'
+$randomPswd = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 16 | % {[char]$_})
+
+$certPwd = ConvertTo-SecureString -String $randomPswd -AsPlainText -Force
+Export-PfxCertificate -Cert $certPath -FilePath $certPfx -Password $certPwd
+
+$appGwObj = Get-AzureRmApplicationGateway -ResourceGroupName $rgName -Name applicationGateway1
+
+Remove-AzureRmApplicationGatewaySslCertificate -ApplicationGateway $appGwObj -Name appGatewaySslCert
+
+Add-AzureRmApplicationGatewaySslCertificate -ApplicationGateway $appGwObj -Name appGatewaySslCert -CertificateFile $certPfx -Password $randomPswd
+
+Set-AzureRmApplicationGateway -ApplicationGateway $appGwObj
+
+
+
+
