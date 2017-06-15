ee0897b23dd352d2c5d5565636df327b3c707fd2
Bill Dall <bdall@teradici.com>
2017-06-15 09:38:43 -0700

    Remove dead code.

--- dev/domain-controller/new-cm-sg-ls-vm/cm_setup_0.1.sh
+++ dev/domain-controller/new-cm-sg-ls-vm/cm_setup_0.1.sh
@@ -2,13 +2,10 @@
 #
 # There must be three arguments passed to this script.
 # first argument is the FQDN of the broker.
-# second argument is the activation code for the license server
 
-#get the install zip files
-wget https://teradeploy.blob.core.windows.net/binaries/P-CM-1.6_SG-1.12.zip -P /tmp/
-wget https://teradeploy.blob.core.windows.net/binaries/SC_1.0.zip -P /tmp/
 
 #Install and setup the Sumo Collector
+wget https://teradeploy.blob.core.windows.net/binaries/SC_1.0.zip -P /tmp/
 unzip -o /tmp/SC_1.0.zip
 rpm -i sumo/SumoCollector-19.182-25.x86_64.rpm
 cp sumo/user.properties /opt/SumoCollector/config/
@@ -21,6 +18,8 @@ service collector restart
 service collector status
 
 
+#get the install zip files
+wget https://teradeploy.blob.core.windows.net/binaries/P-CM-1.6_SG-1.12.zip -P /tmp/
 unzip -o /tmp/P-CM-1.6_SG-1.12.zip
 
 #remove unwanted jdk's
@@ -32,9 +31,6 @@ iptables -I INPUT 1 -p tcp --dport 22 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 4172 -j ACCEPT
-#iptables -I INPUT 1 -p tcp --dport 8090 -j ACCEPT   # for LM Web GUI
-iptables -I INPUT 1 -p tcp --dport 27000 -j ACCEPT  # for LM licensing Port
-iptables -I INPUT 1 -p tcp --dport 27001 -j ACCEPT  # for LM vendor daemon Port
 iptables -I INPUT 1 -p udp --dport 4172 -j ACCEPT
 service iptables save
 
@@ -59,10 +55,6 @@ service connection_manager stop
 # modify CM setup
 #make the 'original' file
 cp -n /etc/ConnectionManager.conf /etc/ConnectionManager.conf.orig
-#awk -v broker=$1 '/^PcoipAddress/{printf "PcoipAddress = %s\n",broker;next};{print}' /etc/ConnectionManager.conf.orig | \
-#awk  -v lserver="27000@$myprivateip" '/^LicenseServerAddress/{printf "LicenseServerAddress = %s\n",lserver;next};{print}'  \
-#    > ConnectionManager.conf
-
 awk -v broker=$1 '/^PcoipAddress/{printf "PcoipAddress = %s\n",broker;next};{print}' /etc/ConnectionManager.conf.orig > ConnectionManager.conf
 
 
@@ -99,54 +91,18 @@ if [ -n "$mypublicip" ]; then
 fi
 
 
-# now setup license server
-#
-#sudo rm -rf /opt/FNPLicenseServerManager
-#unzip -o /tmp/P-LS_1.1.0.zip
-#yum -y install redhat-lsb.i686
-#mkdir license-server_1.1
-#mv license-server_1.1.0.tar.gz license-server_1.1/
-#cd license-server_1.1/
-#tar xvf license-server_1.1.0.tar.gz
-#awk '{ sub(/-i console/,"-i silent\nsleep 120\nchmod +x /opt/FNPLicenseServerManager/lmadmin"); print }' install.sh > install-silent.sh
-#sh install-silent.sh > lm_install.log
 
-#service lmadmin stop
 
-# setup vendor daemon port#
-# copy original config file to temp location
-#cp -f /opt/FNPLicenseServerManager/conf/server.xml /opt/FNPLicenseServerManager/conf/server_orig.xml
-#rewrite original with new port#
-#awk '{ sub(/name="TERADICI" port="0"/,"name=\"TERADICI\" port=\"27001\""); print }' /opt/FNPLicenseServerManager/conf/server_orig.xml > /opt/FNPLicenseServerManager/conf/server.xml
-
-#activate licenses
-#cd /opt/FNPLicenseServerManager/utils
-#./pcoip-activate-license.sh -k "$2" -c 1
-
-#to return license
-# need to find license, use  ./pcoip-view-license.sh and look for the line with "Fulfillment ID:" in it
-# then:
-#./pcoip-return-license.sh -f fulfillment_ID
-
-# reboot now
-# shutdown -r +1 "Server will restart in 1 minute."
-
-#or just restart all the services
+#restart all the services
 service security_gateway restart
 service connection_manager restart
-#service lmadmin restart
 
-#Hack... try to restart the CM after 60 seconds to ensure that the info record is remapped to ROOT. This does not seem to be happening on its own.
-
+#Hack... try to restart the CM again after 60 seconds to ensure that the info record is remapped to ROOT. This does not seem to be happening on its own.
 sleep 60
 service connection_manager restart
 
 
 exit 0
 
-# /opt/FNPLicenseServerManager/conf/server.xml
-#    <vendorDaemons>
-#      <daemon dateBasedVersion="false" dlog="false" executable="TERADICI/TERADICI" license="licenses/TERADICI/Teradici_license_dummy.lic" logFile="logs/TERADICI.log" logOverwrite="false" name="TERADICI" port="27001" restartRetries="10"/>
-#    </vendorDaemons>
 
 
