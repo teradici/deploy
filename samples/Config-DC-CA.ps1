50f91b02f1f1b31fcc8ce8b5a0b723fb663fbb7d
Frank Wang <fwang@teradici.com>
2017-10-25 16:47:27 -0700

    TSW-67105 deploy AD DS

---
+++ samples/Config-DC-CA.ps1
@@ -0,0 +1,133 @@
+﻿configuration ConfigDCCA 
+{ 
+   param 
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$serviceAccountCreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    ) 
+    
+#    Import-DscResource -ModuleName xActiveDirectory,xDisk, xNetworking, cDisk, PSDesiredStateConfiguration, xAdcsDeployment
+    Import-DscResource -ModuleName xActiveDirectory,xDisk, xNetworking, PSDesiredStateConfiguration, xAdcsDeployment, cDisk
+#    Import-DscResource -ModuleName cDisk
+
+#    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("$DomainName\$Admincreds.UserName", $Admincreds.Password)
+	$DomainCreds = $AdminCreds
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager 
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+	    WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"		
+        }
+
+	    WindowsFeature RSAT
+	    {
+	        Ensure = "Present"
+            Name = "RSAT"
+	    }
+
+	    WindowsFeature DnsTools
+	    {
+	        Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+	    }
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+	        DependsOn = "[WindowsFeature]DNS","[WindowsFeature]RSAT","[WindowsFeature]DnsTools"
+        }
+
+        xWaitforDisk Disk2
+        {
+             DiskNumber = 2
+             RetryIntervalSec =$RetryIntervalSec
+             RetryCount = $RetryCount
+	 		 #Make sure all the modules are installed before proceeding - this may not be needed...
+	 		 DependsOn = "[xDnsServerAddress]DnsServerAddress"
+        }
+
+        cDiskNoRestart ADDataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = "F"
+			DependsOn="[xWaitforDisk]Disk2"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+	        DependsOn="[cDiskNoRestart]ADDataDisk"
+        } 
+         
+        xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+	        DependsOn = "[WindowsFeature]ADDSInstall","[xDnsServerAddress]DnsServerAddress"
+        }
+
+        WindowsFeature ADCS-Cert-Authority
+        {
+               Ensure = 'Present'
+               Name = 'ADCS-Cert-Authority'
+               DependsOn = '[xADDomain]FirstDS'
+        }
+
+        xADCSCertificationAuthority ADCS
+        {
+            Ensure = 'Present'
+            Credential = $DomainCreds
+            CAType = 'EnterpriseRootCA'
+            DependsOn = '[WindowsFeature]ADCS-Cert-Authority'
+        }
+        WindowsFeature ADCS-Web-Enrollment
+        {
+            Ensure = 'Present'
+            Name = 'ADCS-Web-Enrollment'
+            DependsOn = '[WindowsFeature]ADCS-Cert-Authority'
+        }
+        xADCSWebEnrollment CertSrv
+        {
+            Ensure = 'Present'
+            IsSingleInstance = 'Yes'
+            Credential = $DomainCreds
+            DependsOn = '[WindowsFeature]ADCS-Web-Enrollment','[xADCSCertificationAuthority]ADCS'
+        }
+
+		xADUser AccountToJoinDomain
+		{
+            Ensure = "Present"
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+			UserName = $serviceAccountCreds.UserName
+			Password = $serviceAccountCreds
+			DependsOn  = "[xADDomain]FirstDS"
+		}
+	}
+}
---
+++ samples/ConfigureWinRM.ps1
@@ -0,0 +1,139 @@
+﻿#################################################################################################################################
+#  Name        : Configure-WinRM.ps1                                                                                            #
+#                                                                                                                               #
+#  Description : Configures the WinRM on a local machine                                                                        #
+#                                                                                                                               #
+#  Arguments   : HostName, specifies the FQDN of machine or domain                                                           #
+#################################################################################################################################
+
+param
+(
+    [string] $hostname,
+	[string] $svrAccountName
+)
+
+#################################################################################################################################
+#                                             Helper Functions                                                                  #
+#################################################################################################################################
+
+function Delete-WinRMListener
+{
+    $config = Winrm enumerate winrm/config/listener
+    foreach($conf in $config)
+    {
+        if($conf.Contains("HTTPS"))
+        {
+            Write-Verbose "HTTPS is already configured. Deleting the exisiting configuration."
+
+            winrm delete winrm/config/Listener?Address=*+Transport=HTTPS
+            break
+        }
+    }
+}
+
+function Get-Thumbprint
+{
+    param([string] $hostname)
+
+	$certLoc = 'cert:Localmachine\My'
+    
+    $thumbprint = (Get-ChildItem $certLoc | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1).Thumbprint
+    
+    $thumbprint
+}
+
+function Create-Certificate
+{
+    param([string] $hostname)
+
+	# create self signed certificate
+	$certLoc = 'cert:Localmachine\My'
+  	$startDate = [DateTime]::Now.AddDays(-1)
+  	$endDate = [DateTime]::Now.AddDays(365)
+   	$subject = "CN=" + $hostname
+   	$cert = New-SelfSignedCertificate -Type Custom -certstorelocation $certLoc -Subject $subject `
+    		-NotBefore $startDate -NotAfter $endDate -KeySpec KeyExchange -KeyExportPolicy Exportable `
+    		-Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" `
+    		-TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1")  
+    
+    $thumbprint=Get-Thumbprint($hostname)
+    if(-not $thumbprint)
+    {
+        throw "Failed to create the test certificate."
+    }
+    
+    $cert
+}
+
+function Configure-WinRMHttpsListener
+{
+    param([string] $hostname)
+
+    # Delete the WinRM Https listener if it is already configured
+    Delete-WinRMListener
+
+    $thumbprint = Get-Thumbprint($hostname)
+    if(-not $thumbprint)
+    {
+    	Create-Certificate($hostname)
+        $thumbprint = Get-Thumbprint($hostname)
+    }	
+
+    $winargs = '@{Hostname="'+$hostname+'";CertificateThumbprint="'+$thumbprint+'"}'
+    winrm create winrm/config/Listener?Address=*+Transport=HTTPS $winargs
+}
+
+function Add-FirewallException
+{
+    param([string] $port)
+
+    # Delete an exisitng rule
+    netsh advfirewall firewall delete rule name="Windows Remote Management (HTTPS-In)" dir=in protocol=TCP localport=$port
+
+    # Add a new firewall rule
+    netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=$port
+}
+
+
+function Delegate-Join-VM-Domain
+{
+    param([string] $saAccount)
+
+	$domain = Get-ADDomain
+	$ouDN = $domain.ComputersContainer
+
+	$SearchAccount = Get-ADUser $saAccount
+ 
+	$SAM = $SearchAccount.SamAccountName
+	$UserAccount = $domain.NetBIOSName+"\"+$SAM
+
+	dsacls.exe $ouDN /G $UserAccount":CCDC;Computer" /I:T | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":LC;;Computer" /I:S | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":RC;;Computer" /I:S | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":WD;;Computer" /I:S  | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":WP;;Computer" /I:S  | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":RP;;Computer" /I:S | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":CA;Reset Password;Computer" /I:S | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":CA;Change Password;Computer" /I:S | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":WS;Validated write to service principal name;Computer" /I:S | Out-Null
+	dsacls.exe $ouDN /G $UserAccount":WS;Validated write to DNS host name;Computer" /I:S | Out-Null
+}
+
+#################################################################################################################################
+#                                              Configure WinRM                                                                  #
+#################################################################################################################################
+
+$winrmHttpsPort=5986
+
+#Configure https listener
+Configure-WinRMHttpsListener $hostname
+
+# Add firewall exception
+Add-FirewallException -port $winrmHttpsPort
+
+#Enable-PSRemoting –force
+#Set-WSManQuickConfig -UseSSL -force
+
+Delegate-Join-VM-Domain $svrAccountName
+#################################################################################################################################
+#################################################################################################################################
