4ef20679618b967873b61435b52f5ab6b0d08dec
Bill Dall <bdall@teradici.com>
2018-01-04 11:03:53 -0800

    Properly resolve host private IP. Increase debug prints.

--- connection-service/new-cm-sg-ls-vm/cm_setup_0.1.sh
+++ connection-service/new-cm-sg-ls-vm/cm_setup_0.1.sh
@@ -3,10 +3,10 @@
 # There must be one argument passed to this script.
 # first argument is the FQDN of the broker.
 
-#Update system to latest
+# Update system to latest
 yum -y update
 
-#Install and setup the Sumo Collector
+# Install and setup the Sumo Collector
 mkdir /tmp/sumo
 wget "https://collectors.sumologic.com/rest/download/linux/64" -O /tmp/sumo/SumoCollector.sh && sudo chmod +x /tmp/sumo/SumoCollector.sh
 wget "$3/user.properties" -O /tmp/sumo/user.properties
@@ -24,15 +24,15 @@ service collector restart
 service collector status
 
 
-#get the install zip files
+# get the install zip files
 wget https://teradeploy.blob.core.windows.net/binaries/CM-1.8_SG-1.14.zip -P /tmp/
 unzip -o /tmp/CM-1.8_SG-1.14.zip
 
-#remove unwanted jdk's
+# remove unwanted jdk's
 yum -y remove java-1.6.0-openjdk
 yum -y remove java-1.7.0-openjdk
 
-#open required ports
+# open required ports
 iptables -I INPUT 1 -p tcp --dport 22 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
@@ -50,21 +50,23 @@ else
 fi
 
 
-#get current networking configuration and use that for system setup. If there are dynamic IP's in the system THIS WILL EVENTUALLY FAIL.
+# get current networking configuration and use that for system setup. If there are dynamic IP's in the system THIS WILL EVENTUALLY FAIL.
+# alternates if opendns turns off: curl ipinfo.io/ip    curl ipecho.net/plain ; echo
 if [ ${sge} = "true" ]
 then
+	echo "Enabling PCoIP security gateway"
 	mypublicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 else
+	echo "Not enabling PCoIP security gateway"
 	mypublicip= "Did not search."
 fi
 
-#alternates if opendns turns off: curl ipinfo.io/ip    curl ipecho.net/plain ; echo
 myhostname=$(hostname)
-myprivateip=$(ifconfig eth0 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}')
+myprivateip=$(ifconfig eth0 | awk '/inet / {gsub("addr:", "", $2); print $2}')
 
 echo "Detected hostname= $myhostname privateIP = $myprivateip publicip = $mypublicip"
 
-#add local IP and name to hosts file.
+# add local IP and name to hosts file.
 echo " $myprivateip   $myhostname" >> /etc/hosts
 service network restart
 
@@ -88,7 +90,8 @@ service security_gateway stop
 service connection_manager stop
 
 # modify CM setup
-#make the 'original' file
+#
+# make the 'original' file
 cp -n /etc/ConnectionManager.conf /etc/ConnectionManager.conf.orig
 awk -v broker="$1" '/^PcoipAddress/{printf "PcoipAddress = %s\n",broker;next};{print}' /etc/ConnectionManager.conf.orig > ConnectionManager.conf
 
@@ -98,10 +101,10 @@ cp -f ConnectionManager.conf /etc/ConnectionManager.conf
 
 echo "Setting up Tomcat to allow an unencrypted link over port 8080"
 
-#make the 'original' file
+# make the 'original' file
 cp -n /opt/Teradici/thirdparty/tomcat/conf/server.xml /opt/Teradici/thirdparty/tomcat/conf/server.xml.orig
 
-#add port 8080 http listener and make the 'info' page available at 'root' to not confuse L7 load balancers.
+# add port 8080 http listener and make the 'info' page available at 'root' to not confuse L7 load balancers.
 awk  '/<Service name=\"Catalina\">/{printf "<Service name=\"Catalina\">\n<Connector port=\"8080\" protocol=\"HTTP/1.1\" connectionTimeout=\"20000\" redirectPort=\"443\" />\n";next};{print}' \
  /opt/Teradici/thirdparty/tomcat/conf/server.xml.orig | \
 awk '/xmlValidation=\"false\" xmlNamespaceAware=\"false\"/{print $0; print "<Context path=\"\" docBase=\"/opt/Teradici/thirdparty/tomcat/webapps/info\" />\n";next}1' \
@@ -114,11 +117,11 @@ echo "Finished setting up CM without SG"
 if [ ${sge} = "true" ]; then
 	if [ -n "$mypublicip" ]; then
 
-		#first, enable sg in CM config and copy it back again
+		# first, enable sg in CM config and copy it back again
 		awk '/^SecurityGatewayEnabled/{printf "SecurityGatewayEnabled = true\n";next};{print}'  /etc/ConnectionManager.conf >  ConnectionManager.conf
 		cp -f ConnectionManager.conf /etc/ConnectionManager.conf
 
-		#make the 'original' file one time only
+		# make the 'original' file one time only
 		cp -n /etc/SecurityGateway.conf /etc/SecurityGateway.conf.orig
 		awk -v externalip="$mypublicip" '/^ExternalRoutableIP/{printf "ExternalRoutableIP = %s\n",externalip;next};{print}' /etc/SecurityGateway.conf.orig > SecurityGateway.conf
 		cp -f SecurityGateway.conf /etc/SecurityGateway.conf
@@ -128,11 +131,11 @@ if [ ${sge} = "true" ]; then
 fi
 
 
-#restart all the services
+# restart all the services
 service security_gateway restart
 service connection_manager restart
 
-#Hack... try to restart the CM again after 60 seconds to ensure that the info record is remapped to ROOT. This does not seem to be happening on its own.
+# Hack... try to restart the CM again after 60 seconds to ensure that the info record is remapped to ROOT. This does not seem to be happening on its own.
 sleep 60
 service connection_manager restart
 
