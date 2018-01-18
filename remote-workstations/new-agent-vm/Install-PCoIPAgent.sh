c7043568e167b63b7c9649c704c7dc7cb1853e95
TERADICI\fwang <fwang@teradici.com>
2018-01-18 10:19:38 -0800

    install nvidia driver

--- remote-workstations/new-agent-vm/Install-PCoIPAgent.sh
+++ remote-workstations/new-agent-vm/Install-PCoIPAgent.sh
@@ -1,57 +1,5 @@
 #!/bin/bash
 
-install_driver()
-{
-    # Install kernel-source/kernel-devel and gcc
-    sudo yum -y install kernel-source kernel-devel gcc
-    FILE_NAME="NVIDIA-Linux-x86_64-367.106-grid.run"
-    FILE_LOCATION="/root/$FILE_NAME"
-    # Download Driver first
-    echo "Downloading and Installing Nvidia driver"
-    sudo wget --retry-connrefused --tries=3 --waitretry=5 -O $FILE_LOCATION https://binarystore.blob.core.windows.net/thirdparty/nvidia/$FILE_NAME
-    exitCode=$?
-    if [ $exitCode -ne 0 ]
-    then
-        echo "failed to download Nvidia driver."
-        # let's define exit code 103 for this case
-        exit 103
-    fi
-    # Change file permission
-    sudo chmod 744 $FILE_LOCATION
-    # run installer
-    sudo $FILE_LOCATION -Z -X -s
-    exitCode=$?
-    
-    if [ $exitCode -eq 0 ]
-    then
-        echo "Driver is installed successfully"
-    else
-        echo "failed to install Nvidia driver. Will create a script to install driver when machine boots up"
-        file_path="/root/install_driver.sh"
-        cat <<EOF >$file_path
-#!/bin/bash
-if [ -e /root/.first_boot ]
-then
-    if [ -e /root/.second_boot ]
-    then
-        exit 0
-    else
-        touch /root/.second_boot
-        sudo $FILE_LOCATION -Z -X -s        
-    fi
-else
-    touch /root/.first_boot
-    sudo $FILE_LOCATION -Z -X -s
-    (sleep 2;  sudo shutdown -f -r +0)&
-fi
-EOF
-        sudo chmod 744 $file_path
-        (crontab -l 2>/dev/null; echo "@reboot $file_path") | crontab -
-        # let's define exit code 104 for this case
-        #exit 104
-    fi
-}
-
 # the first argument is the Registration Code of PCoIP agent
 REGISTRATION_CODE="$1"
 # the second argument is the agent type
@@ -77,46 +25,88 @@ SAS_TOKEN="${11}"
 # the twelfth argument is the Computer OU string
 OU="${12}"
 
-# Make sure Linux OS is up to date
-echo "--> Updating Linux OS to latest"
-# Exclude WALinuxAgent due to it failing to update from within an Azure Custom Script
-sudo yum -y update --exclude=WALinuxAgent
+update_kernel_dkms() 
+{
+  sudo yum -y update
 
-# If it's graphic agent, install Nvidia Driver
-case "$AGENT_TYPE" in 
-    "Graphics")
-        install_driver
-        AGENT_TYPE='graphics'
-        ;;
-    "Standard")
-        AGENT_TYPE='standard'
-        ;;   
-    *)
-        echo "unknown agent type $AGENT_TYPE."
-        # let's define exit code 105 for this case
-        exit 105
-        ;;       
-esac
+  sudo yum -y install kernel-devel
 
-# Join domain
-echo "-->Install required packages to join domain"
-sudo yum -y install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python
-sudo systemctl enable sssd
+  sudo rpm -Uvh --quiet https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-echo "-->Joining the domain"
-if [ -n "${OU}" ]
-then
-    echo "$PASSWORD" | sudo realm join --user="$USERNAME" --computer-ou="${OU}" "$DOMAIN_NAME" >&2
-else
-    echo "$PASSWORD" | sudo realm join --user="$USERNAME" "$DOMAIN_NAME" >&2
-fi
-if [ $? -eq 0 ]
-then
-    echo "Joined Domain ${DOMAIN_NAME} and OU ${OU}"
-else
-    echo "Failed to join Domain ${DOMAIN_NAME} and OU ${OU}"
-    exit 106
-fi
+  sudo yum -y install dkms
+}
+
+# need to reboot after install
+disable_nouveal()
+{
+    echo 'blacklist nouveau' | sudo tee -a /etc/modprobe.d/nouveau.conf
+    
+    echo 'blacklist lbm-nouveau' | sudo tee -a /etc/modprobe.d/nouveau.conf
+}
+
+# need to reboot after install Linux Integration Services for Hyper-V
+install_lis()
+{    
+    local LIS_FILE="lis-rpms-4.2.3-4.tar.gz"
+    
+    wget --retry-connrefused --tries=3 --waitretry=5  http://download.microsoft.com/download/6/8/F/68FE11B8-FAA4-4F8D-8C7D-74DA7F2CFC8C/$LIS_FILE
+
+    tar xvzf $LIS_FILE
+
+    cd LISISO
+    
+    sudo ./install.sh
+    
+    local exitCode=$?
+
+    cd ..
+    
+    return $exitCode
+}
+
+install_nvidia_driver()
+{
+    local FILE_NAME="NVIDIA-Linux-x86_64-384.73-grid.run"
+    
+    wget --retry-connrefused --tries=3 --waitretry=5  https://teradeploy.blob.core.windows.net/binaries/$FILE_NAME  
+
+    chmod +x $FILE_NAME
+
+    sudo ./$FILE_NAME -Z -X -s
+
+    local exitCode=$?
+
+    if [ $exitCode -eq 0 ] 
+    then
+        sudo cp /etc/nvidia/gridd.conf.template /etc/nvidia/gridd.conf
+        
+        echo 'IgnoreSP=TRUE' | sudo tee -a /etc/nvidia/gridd.conf
+    fi
+    
+    return $exitCode
+}
+
+join_domain()
+{
+    # Join domain
+    echo "-->Install required packages to join domain"
+    sudo yum -y install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python
+    sudo systemctl enable sssd
+
+    echo "-->Joining the domain"
+    if [ -n "${OU}" ]
+    then
+        echo "$PASSWORD" | sudo realm join --user="$USERNAME" --computer-ou="${OU}" "$DOMAIN_NAME" >&2
+    else
+        echo "$PASSWORD" | sudo realm join --user="$USERNAME" "$DOMAIN_NAME" >&2
+    fi
+    if [ $? -eq 0 ]
+    then
+        echo "Joined Domain ${DOMAIN_NAME} and OU ${OU}"
+    else
+        echo "Failed to join Domain ${DOMAIN_NAME} and OU ${OU}"
+        return 106
+    fi
 
 echo "-->Configuring settings"
 sudo sed -i '$ a\dyndns_update = True\ndyndns_ttl = 3600\ndyndns_refresh_interval = 43200\ndyndns_update_ptr = True\nldap_user_principal = nosuchattribute' /etc/sssd/sssd.conf
@@ -243,131 +233,334 @@ active_d = ldap_lib(args.address, args.user_name, args.password, args.domain)
 active_d.add_computer_to_group(args.computer, args.group)
 
 EOF
-sudo python $file_path -d "$DOMAIN_NAME" -a "$DC_ADDRESS" -u "$USERNAME" -p "$PASSWORD" -c "$VM_NAME" -g "$GROUP"
 
+    sudo python $file_path -d "$DOMAIN_NAME" -a "$DC_ADDRESS" -u "$USERNAME" -p "$PASSWORD" -c "$VM_NAME" -g "$GROUP"
+}
 
-# Install the EPEL repository
-echo "-->Install the EPEL repository"
-sudo rpm -Uvh --quiet https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+install_gui() 
+{
+    # Make sure Linux OS is up to date
+    echo "--> Updating Linux OS to latest"
 
-# Install the Teradici package key
-echo "-->Install the Teradici package key"
-sudo rpm --import https://downloads.teradici.com/rhel/teradici.pub.gpg
+    # Exclude WALinuxAgent due to it failing to update from within an Azure Custom Script
+    sudo yum -y update --exclude=WALinuxAgent
 
-# Add the Teradici repository
-echo "-->Add the Teradici repository"
+    # Install Desktop
+    echo "-->Install desktop"	
+    #sudo yum -y groupinstall 'X Window System' 'GNOME'
+    sudo yum -y groupinstall "Server with GUI"
+    
+    # sudo yum groups mark convert
 
-agent_repo_url="https://downloads.teradici.com/rhel/pcoip.repo"
-case "$AGENT_CHANNEL" in 
-    "beta")
-		agent_repo_url="https://downloads.teradici.com/rhel/pcoip-beta.repo"
-        ;;
-    "dev")
-		agent_repo_url="https://downloads.teradici.com/rhel/pcoip-dev.repo"
-        ;;   
-    *)
-		agent_repo_url="https://downloads.teradici.com/rhel/pcoip.repo"
-        ;;       
-esac
+    # install firefox
+    echo "-->Install firefox"
+    sudo yum -y install firefox
+    
+    #echo "-->set default graphical target"
+    # The below command will change runlevel from runlevel 3 to runelevel 5 
+    # sudo systemctl set-default graphical.target
+    
+    #echo "-->start graphical target"
+    # sudo systemctl start graphical.target	
+}	
 
-sudo wget --retry-connrefused --tries=3 --waitretry=5 -O /etc/yum.repos.d/pcoip.repo $agent_repo_url
+install_pcoip_agent()
+{
+    # Install the Teradici package key
+    echo "-->Install the Teradici package key"
+    sudo rpm --import https://downloads.teradici.com/rhel/teradici.pub.gpg
+    
+    # Add the Teradici repository
+    echo "-->Add the Teradici repository"
+    
+    agent_repo_url="https://downloads.teradici.com/rhel/pcoip.repo"
+    case "$AGENT_CHANNEL" in 
+        "beta")
+            agent_repo_url="https://downloads.teradici.com/rhel/pcoip-beta.repo"
+            ;;
+        "dev")
+            agent_repo_url="https://downloads.teradici.com/rhel/pcoip-dev.repo"
+            ;;   
+        *)
+            agent_repo_url="https://downloads.teradici.com/rhel/pcoip.repo"
+            ;;       
+    esac
+    
+    sudo wget --retry-connrefused --tries=3 --waitretry=5 -O /etc/yum.repos.d/pcoip.repo $agent_repo_url
+    
+    local exitCode=$?
+    if [ $exitCode -ne 0 ]
+    then
+        echo "failed to add teradici repository."
+        # let's define exit code 100 for this case
+        return 100
+    fi
+    
+    # Install the EPEL repository	
+    #echo "-->Install the EPEL repository"
+    sudo rpm -Uvh --quiet https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-exitCode=$?
-if [ $exitCode -ne 0 ]
+    sudo yum -y update --exclude=WALinuxAgent
+        
+    # Install the PCoIP Agent
+    echo "-->Install the PCoIP $AGENT_TYPE agent"
+    for idx in {1..3}
+    do
+        sudo yum -y install pcoip-agent-$AGENT_TYPE
+        exitCode=$?
+        
+        if [ $exitCode -eq 0 ]
+        then
+            break
+        else
+            #delay 5 seconds
+            sleep 5
+            sudo yum -y remove pcoip-agent-$AGENT_TYPE
+            if [ $idx -eq 3 ]
+            then
+                echo "failed to install pcoip agent."
+                # let's define exit code 101 for this case
+                return 101
+            fi
+            #delay 5 seconds        
+            sleep 5
+        fi
+    done
+        
+    return 0
+}
+
+register_pcoip_license() 
+{
+    # register license code
+    echo "-->Register license code"
+    for idx in {1..5}
+    do
+        pcoip-register-host --registration-code="$REGISTRATION_CODE"
+        pcoip-validate-license    
+        local exitCode=$?
+        
+        if [ $exitCode -eq 0 ]
+        then
+            break
+        else
+            if [ $idx -eq 5 ]
+            then
+                echo "failed to register pcoip agent license."
+                # let's define exit code 102 for this case
+                return 102
+            fi
+            sleep 10
+        fi
+    done		
+    
+    return 0
+}
+
+install_SumoLogic() 
+{
+    # Install and setup the Sumo Collector
+    echo "-->Install SumoLogic collector"
+    mkdir /tmp/sumo
+    wget "https://collectors.sumologic.com/rest/download/linux/64" -O /tmp/sumo/SumoCollector.sh && sudo chmod +x /tmp/sumo/SumoCollector.sh
+    wget "$STORAGEURI/user.properties$SAS_TOKEN" -O /tmp/sumo/user.properties
+    wget "$STORAGEURI/sumo-agent-vm-linux.json$SAS_TOKEN" -O /tmp/sumo/sumo-agent-vm-linux.json
+    JSON_FILE=/tmp/sumo/sumo-agent-vm-linux.json
+    echo "Attemtping to set sumo collector ID to: " "$COLLECTOR_ID"
+    sed -i s/collectorID/"$COLLECTOR_ID"/ /tmp/sumo/user.properties
+    sed -i 's|syncsourceFile|'$JSON_FILE'|' /tmp/sumo/user.properties
+
+    sudo /tmp/sumo/SumoCollector.sh -q -varfile user.properties
+
+    # service collector install - configures the collector to start at boot time
+    service collector install
+    service collector restart
+    service collector status
+}
+
+install_idle()
+{
+    # Install idle shutdown script
+    echo "-->Install idle shutdown"
+    mkdir /tmp/idleShutdown
+    wget "$STORAGEURI/Install-Idle-Shutdown.sh$SAS_TOKEN" -O /tmp/idleShutdown/Install-Idle-Shutdown-raw.sh 
+    awk '{ sub("\r$", ""); print }' /tmp/idleShutdown/Install-Idle-Shutdown-raw.sh > /tmp/idleShutdown/Install-Idle-Shutdown.sh && sudo chmod +x /tmp/idleShutdown/Install-Idle-Shutdown.sh
+    sudo /tmp/idleShutdown/Install-Idle-Shutdown.sh -install
+}
+
+exit_restart()
+{
+    (sleep 1; sudo reboot) &
+    exit;	
+}
+
+# run start from here
+
+AGENT_TYPE_STANDARD="standard"
+AGENT_TYPE_GRAPHICS="graphics"
+
+#define global EXIT_CODE variable
+EXIT_CODE=1
+INST_LOG_FILE=/root/install_pcoip_agent.status
+INST_LAST_STEP="initial"
+
+AGENT_CHANNEL=$(tr '[:upper:]' '[:lower:]' <<<"$AGENT_CHANNEL")
+AGENT_TYPE=$(tr '[:upper:]' '[:lower:]' <<<"$AGENT_TYPE")
+
+if [ "$AGENT_TYPE" != "$AGENT_TYPE_STANDARD" ] && [ "$AGENT_TYPE" != "$AGENT_TYPE_GRAPHICS"  ]
 then
-    echo "failed to add teradici repository."
-    # let's define exit code 100 for this case
-    exit 100
+    echo "unknown agent type $AGENT_TYPE."
+    # let's define exit code 105 for this case
+    exit 105
 fi
 
-# Install the PCoIP Agent
-echo "-->Install the PCoIP $AGENT_TYPE agent"
-for idx in {1..3}
-do
-    sudo yum -y install pcoip-agent-$AGENT_TYPE
-    exitCode=$?
+if [ -f $INST_LOG_FILE ]
+then
+    INST_LAST_STEP=$(tail -1 $INST_LOG_FILE) 
+fi
+
+if [ "$INST_LAST_STEP" == "initial" ]
+then
+    echo "start installing pcoip $AGENT_TYPE agent" | tee $INST_LOG_FILE
+
+    echo "step1 starting" | tee -a $INST_LOG_FILE
+
+    sudo yum install wget
+
+    install_SumoLogic
+
+    install_gui
+
+    join_domain
+
+    EXIT_CODE=$?
     
-    if [ $exitCode -eq 0 ]
+    if [ $EXIT_CODE -eq 0 ]
     then
-        break
+        INST_LAST_STEP="step1 done"
     else
-        #delay 5 seconds
-        sleep 5
-        sudo yum -y remove pcoip-agent-$AGENT_TYPE
-        if [ $idx -eq 3 ]
-        then
-            echo "failed to install pcoip agent."
-            # let's define exit code 101 for this case
-            exit 101
-        fi
-        #delay 5 seconds        
-        sleep 5
+        INST_LAST_STEP="step1 failure: $EXIT_CODE"
     fi
-done
-    
 
-# register license code
-echo "-->Register license code"
-for idx in {1..5}
-do
-    pcoip-register-host --registration-code="$REGISTRATION_CODE"
-    pcoip-validate-license    
-    exitCode=$?
+    echo "$INST_LAST_STEP" | tee -a $INST_LOG_FILE
+fi	
+
+if [ "$INST_LAST_STEP" == "step1 done" ]
+then 
+    echo "step2 starting" | tee -a $INST_LOG_FILE
+
+    install_pcoip_agent
     
-    if [ $exitCode -eq 0 ]
+    EXIT_CODE=$?
+    
+    if [ $EXIT_CODE -eq 0 ]
     then
-        break
+        INST_LAST_STEP="step2 done"
     else
-        if [ $idx -eq 5 ]
-        then
-            echo "failed to register pcoip agent license."
-            # let's define exit code 102 for this case
-            exit 102
-        fi
-        sleep 60
+        INST_LAST_STEP="step2 failure: $EXIT_CODE"
     fi
-done
+    
+    echo "$INST_LAST_STEP" | tee -a $INST_LOG_FILE
+fi
 
-# Install and setup the Sumo Collector
-echo "-->Install SumoLogic collector"
-mkdir /tmp/sumo
-wget "https://collectors.sumologic.com/rest/download/linux/64" -O /tmp/sumo/SumoCollector.sh && sudo chmod +x /tmp/sumo/SumoCollector.sh
-wget "$STORAGEURI/user.properties$SAS_TOKEN" -O /tmp/sumo/user.properties
-wget "$STORAGEURI/sumo-agent-vm-linux.json$SAS_TOKEN" -O /tmp/sumo/sumo-agent-vm-linux.json
-JSON_FILE=/tmp/sumo/sumo-agent-vm-linux.json
-echo "Attemtping to set sumo collector ID to: " "$COLLECTOR_ID"
-sed -i s/collectorID/"$COLLECTOR_ID"/ /tmp/sumo/user.properties
-sed -i 's|syncsourceFile|'$JSON_FILE'|' /tmp/sumo/user.properties
+if [ "$INST_LAST_STEP" == "step2 done" ]
+then 
+    echo "step3 starting" | tee -a $INST_LOG_FILE
 
-sudo /tmp/sumo/SumoCollector.sh -q -varfile user.properties
+    register_pcoip_license
+    
+    EXIT_CODE=$?
+    
+    if [ $EXIT_CODE -eq 0 ]
+    then 
+        INST_LAST_STEP="step3 done"			
+    else
+        INST_LAST_STEP="step3 failure: $EXIT_CODE"
+    fi
 
-# service collector install - configures the collector to start at boot time
-service collector install
-service collector restart
-service collector status
+    echo "$INST_LAST_STEP" | tee -a $INST_LOG_FILE
+fi
 
-# Install idle shutdown script
-echo "-->Install idle shutdown"
-mkdir /tmp/idleShutdown
-wget "$STORAGEURI/Install-Idle-Shutdown.sh$SAS_TOKEN" -O /tmp/idleShutdown/Install-Idle-Shutdown-raw.sh 
-awk '{ sub("\r$", ""); print }' /tmp/idleShutdown/Install-Idle-Shutdown-raw.sh > /tmp/idleShutdown/Install-Idle-Shutdown.sh && sudo chmod +x /tmp/idleShutdown/Install-Idle-Shutdown.sh
-sudo /tmp/idleShutdown/Install-Idle-Shutdown.sh -install
+if [ $AGENT_TYPE == $AGENT_TYPE_GRAPHICS ]
+then
+    echo "start installing nvidia driver" | tee -a $INST_LOG_FILE
 
-# Install Desktop
-echo "-->Install desktop"
-# sudo yum -y groupinstall "Server with GUI"
-sudo yum -y groupinstall 'X Window System' 'GNOME'
+    if [ "$INST_LAST_STEP" == "step3 done" ]
+    then
+        echo "step4 starting" | tee -a $INST_LOG_FILE
+        update_kernel_dkms
+    
+        disable_nouveal
+    
+        INST_LAST_STEP="step4 done"
+    
+        echo "$INST_LAST_STEP" | tee -a $INST_LOG_FILE
 
-# install firefox
-echo "-->Install firefox"
-sudo yum -y install firefox
+        #schedule job to continue installation	
+        script_file=$(realpath "$0")
+        chmod +x $script_file
 
-echo "-->set default graphical target"
-# The below command will change runlevel from runlevel 3 to runelevel 5 
-sudo systemctl set-default graphical.target
+        params=""
+        for var in "$@"
+        do
+            params="$params \"$var\""
+        done
 
-echo "-->start graphical target"
-sudo systemctl start graphical.target
+        (sudo crontab -l 2>/dev/null; echo "@reboot bash $script_file  $params") | sudo crontab -
+    
+        #exit and restart VM
+        exit_restart
+    fi	
+    
+    if [ "$INST_LAST_STEP" == "step4 done" ]
+    then
+        echo "step5 starting" | tee -a $INST_LOG_FILE
 
-(sleep 2;  sudo shutdown -f -r +0)&
-exit 0
+        install_lis
+    
+        EXIT_CODE=$?
+    
+        if [ $EXIT_CODE -eq 0 ]
+        then 
+            INST_LAST_STEP="step5 done"
+        else
+            INST_LAST_STEP="step5 failure: $EXIT_CODE"
+        fi
+
+        echo "$INST_LAST_STEP" | tee -a $INST_LOG_FILE
+    
+        if [ $EXIT_CODE -eq 0 ]
+        then	
+            exit_restart
+        fi
+    fi
+
+    if [ "$INST_LAST_STEP" == "step5 done" ]
+    then
+        echo "step6 starting" | tee -a $INST_LOG_FILE
+
+        install_nvidia_driver
+        EXIT_CODE=$?
+    
+        if [ $EXIT_CODE -eq 0 ]
+        then 
+            INST_LAST_STEP="step6 done"
+        else
+            INST_LAST_STEP="step6 failure: $EXIT_CODE"
+        fi
+
+        echo "$INST_LAST_STEP" | tee -a $INST_LOG_FILE
+    fi
+fi
+
+#remove job
+sudo crontab -r
+
+install_idle
+
+if [ $EXIT_CODE -eq 0 ]
+then	
+    (sleep 1; sudo reboot) &
+fi
+
+exit $EXIT_CODE;
