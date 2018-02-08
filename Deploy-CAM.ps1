5173883b083e5e8c560d28c2485da0edc16f699b
Charanpreet Parmar <cparmar@teradici.com>
2018-02-07 17:17:08 -0800

    Fixed issue with admin-rw auto idle shutdown time: * fixed issue where defaultIdleShutdownTime parameter wasn't passed to admin-rw machine

--- Deploy-CAM.ps1
+++ Deploy-CAM.ps1
@@ -2468,6 +2468,9 @@ function Deploy-CAM() {
                 },
                 "secretName": "radiusSharedSecret"
             }
+        },
+        "autoShutdownIdleTime" : {
+            "value": $defaultIdleShutdownTime
         }
     }
 }
